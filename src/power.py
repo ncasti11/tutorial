30803cb0f9e1e3e4ccaabc597005c39697a1bb17
Nacho Castineiras <Ignacio.Castineiras@cit.ie>
2017-06-13 14:50:21 +0100

    Clean Python Program

--- src/power.py
+++ src/power.py
@@ -1,10 +1,36 @@
-def to_the_power(x, y = 2):
-    result = x
-    for i in range(0, y):
-        result = result * x
+# --------------------------------------------------------
+#           PYTHON PROGRAM
+# Here is where we are going to define our set of...
+# - Imports
+# - Global Variables
+# - Functions
+# ...to achieve the functionality required.
+# When executing > python 'this_file'.py in a terminal,
+# the Python interpreter will load our program,
+# but it will execute nothing yet.
+# --------------------------------------------------------
+
+# ------------------------------------------
+#
+# FUNCTION my_function1
+#
+# ------------------------------------------
+def my_function1(a, b):
+    result = a
+    for i in range(0, b):
+        result = result * a
     return result
 
-x = 9
-y = 8 #
-print("{0} to the power of {1} is: {2}".format(x, y, to_the_power(x, y)))
-print("The end")
+# ---------------------------------------------------------------
+#           PYTHON EXECUTION
+# This is the main entry point to the execution of our program.
+# It provides a call to the 'main function' defined in our
+# Python program, making the Python interpreter to trigger
+# its execution.
+# ---------------------------------------------------------------
+if __name__ == '__main__':
+	x = 9
+	y = 8 #
+	res = my_function1(x,y)
+	print(res)
+
